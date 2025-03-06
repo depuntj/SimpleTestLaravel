bc634bc1ccbe418b6ebfb0926c6885177dfd9f78
Admin D <danan12453@gmail.com>
2025-03-06 07:41:10 +0700

    1st task done

--- database/migrations/2025_03_06_002747_create_customers_table.php
+++ database/migrations/2025_03_06_002747_create_customers_table.php
@@ -11,10 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('customers', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+    Schema::create('customers', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->string('email')->unique();
+        $table->string('phone');
+        $table->text('address');
+        $table->timestamps();
+    });
     }
 
     /**
